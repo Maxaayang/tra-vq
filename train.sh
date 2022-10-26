53737dbbd42c50fb19c0f110c18dbd6c4936a8e0
Maxaayang <1362459517@qq.com>
2022-10-26 08:58:46 +0800

    vqvae 125

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 # conda activate col
 
 # python3 train.py config/default.yaml > ./logs/test.log
-python3 train.py config/default.yaml > ./logs/vqvae/vqvae2.log
+python3 train.py config/default.yaml > ./logs/vqvae/vqvae125.log
 
 # python3 generate.py config/default.yaml ./ckpt/juke50/params/step_9850-RC_1.760-KL_0.000-model.pt \
 # generations/juke50/ 10 5 > ./logs/gen50.log
