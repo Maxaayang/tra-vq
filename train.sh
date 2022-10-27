598b147e176908c8cd108c82c598a615718df456
Maxaayang <1362459517@qq.com>
2022-10-27 09:45:18 +0800

    vqvae 400 generation

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 # conda activate col
 
 # python3 train.py config/default.yaml > ./logs/test.log
-rm -rf ./ckpt/vqvae400/*
-python3 train.py config/default.yaml > ./logs/vqvae/vqvae400.log
+# rm -rf ./ckpt/vqvae400/*
+# python3 train.py config/default.yaml > ./logs/vqvae/vqvae400.log
 
 # python3 generate.py config/default.yaml ./ckpt/juke50/params/step_9850-RC_1.760-KL_0.000-model.pt \
 # generations/juke50/ 10 5 > ./logs/gen50.log
@@ -12,6 +12,6 @@ python3 train.py config/default.yaml > ./logs/vqvae/vqvae400.log
 # python train.py config/default.yaml > ./logs/vqvae/new_vqvae50.log
 
 
-# rm -rf ./generations/juke10/*
-# python3 generate.py config/default.yaml ./ckpt/juke10/step_950-RC_2.696-KL_0.000-model.pt \
-# generations/juke10/ 10 5 > ./logs/gen/gen_vq10.log
+rm -rf ./generations/vqvae400/*
+python3 generate.py config/default.yaml ./ckpt/vqvae400/params/step_57200-RC_1.266-VQ_0.083-model.pt \
+generations/vqvae400/ 10 5 > ./logs/gen/gen_vq4000.log
