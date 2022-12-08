8d6f2c03ec490ee8a289a8ecde9f08fdfc64c25c
Maxaayang <1362459517@qq.com>
2022-12-08 10:05:23 +0800

    commit

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 # python3 train.py config/default.yaml > ./logs/test.log
 # python3 train.py config/default.yaml > ./logs/vqvae10.log
 
-rm -rf /home/u21s052015/code/tra-vq/ckpt/or_vae100/*
-python train.py config/default.yaml > ./logs/or_vae/or_vae100.log
+rm -rf /home/u21s052015/code/tra-vq/ckpt/or_vae10/*
+python train.py config/default.yaml > ./logs/or_vae/or_vae10.log
 
 # python3 generate.py config/default.yaml ./ckpt/or_vae10/step_950-RC_2.820-KL_0.010-model.pt \
 # generations/or_vae10/ 10 5 > ./logs/or_gen10.log
